b1e49b2e2f0b165ec4599d841a1ce639c45b6e53
vladislav <vladislav.051094@yandex.ru>
2026-02-24 23:12:06 +0300

    added: get user data from profiles table

--- src/entities/user/actions.ts
+++ src/entities/user/actions.ts
@@ -6,5 +6,15 @@ export async function getUser() {
 	const sb = await createClientServer();
 	const { data } = await sb.auth.getUser();
 
-	return data.user;
+	if (!data.user) {
+		return null;
+	}
+
+	const profile = await sb.from("profiles").select("*").eq("id", data.user.id).single();
+
+	if (!profile.data) {
+		return null;
+	}
+
+	return profile.data;
 }
