9339d2d53958a1ec6a6978b447d0ad86958af1e1
vladislav <vladislav.051094@yandex.ru>
2026-03-17 21:58:13 +0300

    changed: supabase env vars to public

--- src/shared/api/supabase/config.ts
+++ src/shared/api/supabase/config.ts
@@ -1,2 +1,2 @@
-export const SUPABASE_URL = process.env.SUPABASE_URL ?? "";
-export const SUPABASE_KEY = process.env.SUPABASE_PUBLISHABLE_DEFAULT_KEY ?? "";
+export const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL ?? "";
+export const SUPABASE_KEY = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY ?? "";
