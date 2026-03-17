ad3bbd98292ed20d4eb4a0b908b54ae5533ed31c
vladislav <vladislav.051094@yandex.ru>
2026-03-17 22:43:42 +0300

    added: supabse browser client singleton

--- src/shared/api/supabase/client.ts
+++ src/shared/api/supabase/client.ts
@@ -1,7 +1,16 @@
+import type { SupabaseClient } from "@supabase/supabase-js";
 import type { Database } from "./types";
 
 import { createBrowserClient } from "@supabase/ssr";
 
 import { SUPABASE_KEY, SUPABASE_URL } from "./config";
 
-export const createClientBrowser = () => createBrowserClient<Database>(SUPABASE_URL, SUPABASE_KEY);
+let client: SupabaseClient<Database> | undefined;
+
+export const createClientBrowser = () => {
+	if (!client) {
+		client = createBrowserClient<Database>(SUPABASE_URL, SUPABASE_KEY);
+	}
+
+	return client;
+};
