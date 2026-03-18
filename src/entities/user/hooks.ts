7a7812bac00b08f1c821159451fee6cc4d5f49a5
vladislav <vladislav.051094@yandex.ru>
2026-03-18 23:57:56 +0300

    added: user entitie hook to get user

---
+++ src/entities/user/hooks.ts
@@ -0,0 +1,11 @@
+import { useQuery } from "@tanstack/react-query";
+
+import { getQueryOptionsUser } from "./api";
+
+export function useUser() {
+	const { data } = useQuery({
+		...getQueryOptionsUser(),
+	});
+
+	return data ?? null;
+}
