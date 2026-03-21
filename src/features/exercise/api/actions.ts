d4f247864ed65073ce8f72605727d12e8081be20
vladislav <vladislav.051094@yandex.ru>
2026-03-21 18:44:19 +0300

    added: exercise feature form add select category (only single tmp)

--- src/features/exercise/api/actions.ts
+++ src/features/exercise/api/actions.ts
@@ -5,10 +5,11 @@ import { createExercise } from "@Entities/exercise";
 export async function addExerciseAction(_prevState: unknown, formData: FormData) {
 	const name = formData.get("name")?.toString().trim();
 	const description = formData.get("description")?.toString().trim();
+	const categoryIds = formData.get("categoryIds")?.toString().trim();
 
 	if (!name) {
 		return { success: false, error: "Missing name" };
 	}
 
-	return createExercise({ name, description });
+	return createExercise({ name, description, categoryIds: categoryIds ? [categoryIds] : undefined });
 }
--- src/features/exercise/ui/FormAdd.tsx
+++ src/features/exercise/ui/FormAdd.tsx
@@ -2,17 +2,31 @@
 
 import type { ElProps } from "@Shared/types";
 
+import { useTranslations } from "next-intl";
 import { useActionState, useEffect } from "react";
 
-import { Button, Input, Textarea } from "@Shared/ui";
+import {
+	Button,
+	Input,
+	Select,
+	SelectContent,
+	SelectGroup,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
+	Textarea,
+} from "@Shared/ui";
 import { cn } from "@Shared/utils";
+import { useExerciseCategoriesAll } from "@Entities/exercise-category";
 
 import { addExerciseAction } from "../api";
 
 export type FormExerciseAddProps = ElProps<"form">;
 
 export function FormExerciseAdd({ className, ...restProps }: FormExerciseAddProps) {
+	const tExerciseCategories = useTranslations("exerciseCategories");
 	const [state, dispatchAction] = useActionState(addExerciseAction, null);
+	const exerciseCategoriesAll = useExerciseCategoriesAll();
 
 	useEffect(() => {
 		console.log("add exercise state:", state);
@@ -22,6 +36,28 @@ export function FormExerciseAdd({ className, ...restProps }: FormExerciseAddProp
 		<form className={cn("flex flex-col gap-4", className)} action={dispatchAction} {...restProps}>
 			<Input name="name" placeholder="Enter category name" />
 			<Textarea name="description" placeholder="Enter category description" />
+
+			{!!exerciseCategoriesAll.length && (
+				<Select name="categoryIds">
+					<SelectTrigger className="w-full">
+						<SelectValue placeholder="Select category" />
+					</SelectTrigger>
+
+					<SelectContent>
+						<SelectGroup>
+							{exerciseCategoriesAll.map(category => (
+								<SelectItem key={category.id} value={category.id}>
+									{String(
+										category.slug
+											? tExerciseCategories(category.slug as Parameters<typeof tExerciseCategories>[0])
+											: category.name,
+									)}
+								</SelectItem>
+							))}
+						</SelectGroup>
+					</SelectContent>
+				</Select>
+			)}
 
 			<Button type="submit">Add exercise</Button>
 		</form>
