a43c58185b155ae5fca76b68210a0d7e1fde10c6
vladislav <vladislav.051094@yandex.ru>
2026-03-23 20:19:03 +0300

    changed: exercise entitie card with full functions

--- src/entities/exercise/ui/Card.tsx
+++ src/entities/exercise/ui/Card.tsx
@@ -1,64 +1,99 @@
 "use client";
 
 import type { ElProps } from "@Shared/types";
-import type { ExerciseUpdateDTO } from "../model";
+import type { Exercise, ExerciseUpdateDTO } from "../model";
 
+import { useTranslations } from "next-intl";
 import { useEffect, useRef, useState } from "react";
 import { CheckIcon, EditIcon, TrashIcon } from "lucide-react";
 
-import { Badge, Button, Card, CardDescription, CardFooter, CardTitle, Input, Textarea } from "@Shared/ui";
+import {
+	Badge,
+	Button,
+	Card,
+	CardDescription,
+	CardFooter,
+	CardTitle,
+	Input,
+	NativeSelect,
+	NativeSelectOption,
+	Textarea,
+} from "@Shared/ui";
 import { cn } from "@Shared/utils";
+import { useExerciseCategoriesAll } from "@Entities/exercise-category";
 
 import { deleteExercise, updateExercise } from "../api";
 
 export interface CardExerciseProps extends ElProps<"div"> {
-	exerciseId: string;
-	exerciseName: string;
-	exerciseDescription?: string;
-	exerciseCategories?: string[];
-	isEditable?: boolean;
-	isRemovable?: boolean;
+	exercise: Exercise;
 }
 
-export function CardExercise({
-	className,
-	exerciseId,
-	exerciseName,
-	exerciseDescription,
-	exerciseCategories,
-	isEditable,
-	isRemovable,
-	...restProps
-}: CardExerciseProps) {
+export function CardExercise({ className, exercise, ...restProps }: CardExerciseProps) {
 	const refInputName = useRef<HTMLInputElement>(null);
 	const refTextareaDescription = useRef<HTMLTextAreaElement>(null);
+	const refSelectCategories = useRef<HTMLSelectElement>(null);
 
 	const [isEditing, setIsEditing] = useState(false);
+
+	const tExercise = useTranslations("exercises");
+	const tExerciseCategories = useTranslations("exerciseCategories");
+	const exerciseCategoriesAll = useExerciseCategoriesAll();
+
+	const exerciseName = String(
+		exercise.slug ? tExercise(exercise.slug as Parameters<typeof tExercise>[0]) : exercise.name,
+	);
+
+	const isEditable = !exercise.isSystem;
+	const isRemovable = !exercise.isSystem;
 
 	function handleClickEdit() {
 		setIsEditing(true);
 	}
 
 	async function handleClickSave() {
-		const data: Pick<ExerciseUpdateDTO, "name" | "description"> = {};
+		const data: Omit<ExerciseUpdateDTO, "exerciseId"> = {};
 
-		if (refInputName.current?.value && refInputName.current.value !== exerciseName) {
+		if (refInputName.current?.value && refInputName.current.value !== exercise.name) {
 			data.name = refInputName.current.value;
 		}
 
-		if (refTextareaDescription.current?.value && refTextareaDescription.current.value !== exerciseDescription) {
+		if (refTextareaDescription.current && refTextareaDescription.current.value !== exercise.description) {
 			data.description = refTextareaDescription.current.value;
 		}
 
+		if (refSelectCategories.current) {
+			const selectedOptions = Array.from(refSelectCategories.current.selectedOptions).filter(option =>
+				Boolean(option.value),
+			);
+
+			let needUpdate = false;
+
+			if (selectedOptions.length !== exercise.categories.length) {
+				needUpdate = true;
+			} else {
+				for (const option of selectedOptions) {
+					if (!exercise.categories.some(category => category.id === option.value)) {
+						needUpdate = true;
+
+						break;
+					}
+				}
+			}
+
+			if (needUpdate) {
+				data.categoryIds = selectedOptions.map(option => option.value);
+			}
+		}
+
 		if (Object.keys(data).length) {
-			await updateExercise({ exerciseId, ...data });
+			await updateExercise({ exerciseId: exercise.id, ...data });
 		}
 
 		setIsEditing(false);
 	}
 
 	async function handleClickRemove() {
-		await deleteExercise({ exerciseId });
+		await deleteExercise({ exerciseId: exercise.id });
 	}
 
 	useEffect(() => {
@@ -77,28 +112,52 @@ export function CardExercise({
 				)}
 			</CardTitle>
 
-			{exerciseDescription && (
-				<CardDescription>
-					{isEditing ? (
-						<Textarea
-							ref={refTextareaDescription}
-							placeholder="Enter exercise description"
-							defaultValue={exerciseDescription}
-						/>
-					) : (
-						exerciseDescription
-					)}
-				</CardDescription>
+			{!isEditing && exercise.description && <CardDescription>{exercise.description}</CardDescription>}
+
+			{isEditing && (
+				<Textarea
+					ref={refTextareaDescription}
+					placeholder="Enter exercise description"
+					defaultValue={exercise.description ?? undefined}
+				/>
 			)}
 
-			{!!exerciseCategories?.length && (
+			{!isEditing && !!exercise.categories.length && (
 				<div className="flex items-center flex-wrap gap-1">
-					{exerciseCategories.map(category => (
-						<Badge key={category} variant="secondary">
-							{category}
+					{exercise.categories.map(category => (
+						<Badge key={category.id} variant="secondary">
+							{String(
+								category.slug
+									? tExerciseCategories(category.slug as Parameters<typeof tExerciseCategories>[0])
+									: category.name,
+							)}
 						</Badge>
 					))}
 				</div>
+			)}
+
+			{isEditing && !!exerciseCategoriesAll.length && (
+				<NativeSelect
+					ref={refSelectCategories}
+					className="h-24"
+					wrapperClassName="w-full"
+					multiple
+					defaultValue={exerciseCategoriesAll
+						.filter(category => !!exercise.categories.find(c => c.id === category.id))
+						.map(category => category.id)}
+				>
+					<NativeSelectOption value="">Select categories</NativeSelectOption>
+
+					{exerciseCategoriesAll.map(category => (
+						<NativeSelectOption key={category.id} value={category.id}>
+							{String(
+								category.slug
+									? tExerciseCategories(category.slug as Parameters<typeof tExerciseCategories>[0])
+									: category.name,
+							)}
+						</NativeSelectOption>
+					))}
+				</NativeSelect>
 			)}
 
 			{(isEditable || isRemovable) && (
