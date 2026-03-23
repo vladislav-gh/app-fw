656bfde9b2564637b246592edfb725ed84765a8b
vladislav <vladislav.051094@yandex.ru>
2026-03-23 21:04:02 +0300

    changed: exercise-category card logic

--- src/entities/exercise-category/ui/Card.tsx
+++ src/entities/exercise-category/ui/Card.tsx
@@ -1,48 +1,65 @@
 "use client";
 
 import type { ElProps } from "@Shared/types";
+import type { ExerciseCategory } from "../model";
 
+import { useTranslations } from "next-intl";
 import { useEffect, useRef, useState } from "react";
+import { useQueryClient } from "@tanstack/react-query";
 import { CheckIcon, EditIcon, TrashIcon } from "lucide-react";
 
 import { Button, Card, CardTitle, Input } from "@Shared/ui";
 import { cn } from "@Shared/utils";
+import { useUser } from "@Entities/user";
 
-import { deleteExerciseCategory, updateExerciseCategory } from "../api";
+import { deleteExerciseCategory, QUERY_KEYS_EXERCISE_CATEGORY, updateExerciseCategory } from "../api";
 
 export interface CardExerciseCategoryProps extends ElProps<"div"> {
-	categoryId: string;
-	categoryName: string;
-	isEditable?: boolean;
-	isRemovable?: boolean;
+	category: ExerciseCategory;
 }
 
-export function CardExerciseCategory({
-	className,
-	categoryId,
-	categoryName,
-	isEditable,
-	isRemovable,
-	...restProps
-}: CardExerciseCategoryProps) {
+export function CardExerciseCategory({ className, category, ...restProps }: CardExerciseCategoryProps) {
 	const refInputName = useRef<HTMLInputElement>(null);
 
 	const [isEditing, setIsEditing] = useState(false);
+
+	const tExerciseCategories = useTranslations("exerciseCategories");
+	const user = useUser();
+	const queryClient = useQueryClient();
+
+	const categoryName = String(
+		category.slug ? tExerciseCategories(category.slug as Parameters<typeof tExerciseCategories>[0]) : category.name,
+	);
+
+	const isEditable = user && !category.isSystem;
+	const isRemovable = user && !category.isSystem;
 
 	function handleClickEdit() {
 		setIsEditing(true);
 	}
 
 	async function handleClickSave() {
+		if (!user) {
+			return;
+		}
+
 		if (refInputName.current?.value && refInputName.current.value !== categoryName) {
-			await updateExerciseCategory({ categoryId, name: refInputName.current.value });
+			await updateExerciseCategory({ categoryId: category.id, name: refInputName.current.value });
+
+			queryClient.invalidateQueries({ queryKey: QUERY_KEYS_EXERCISE_CATEGORY.user(user.id) });
 		}
 
 		setIsEditing(false);
 	}
 
 	async function handleClickRemove() {
-		await deleteExerciseCategory({ categoryId });
+		if (!user) {
+			return;
+		}
+
+		await deleteExerciseCategory({ categoryId: category.id });
+
+		queryClient.invalidateQueries({ queryKey: QUERY_KEYS_EXERCISE_CATEGORY.user(user.id) });
 	}
 
 	useEffect(() => {
