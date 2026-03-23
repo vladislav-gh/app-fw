98aa9ef937afe3b8071a2dad004a9b6b28860143
vladislav <vladislav.051094@yandex.ru>
2026-03-23 21:56:55 +0300

    added: workout feature form add for first steps

---
+++ src/features/workout/ui/FormAdd.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import type { ElProps } from "@Shared/types";
+
+import { useActionState, useEffect, useEffectEvent } from "react";
+import { useQueryClient } from "@tanstack/react-query";
+
+import { Button, Input, Textarea } from "@Shared/ui";
+import { cn } from "@Shared/utils";
+import { useUser } from "@Entities/user";
+import { QUERY_KEYS_WORKOUT } from "@Entities/workout";
+
+import { addWorkoutAction } from "../api";
+
+export type FormWorkoutAddProps = ElProps<"form">;
+
+export function FormWorkoutAdd({ className, ...restProps }: FormWorkoutAddProps) {
+	const [state, dispatchAction] = useActionState(addWorkoutAction, null);
+	const queryClient = useQueryClient();
+	const user = useUser();
+
+	const handleSuccess = useEffectEvent(() => {
+		if (user && state?.success && state.data) {
+			queryClient.invalidateQueries({ queryKey: QUERY_KEYS_WORKOUT.all(user.id) });
+		}
+	});
+
+	useEffect(() => {
+		handleSuccess();
+	}, [state]);
+
+	if (!user) {
+		return null;
+	}
+
+	return (
+		<form className={cn("flex flex-col gap-4", className)} action={dispatchAction} {...restProps}>
+			<Input type="number" name="userWeight" placeholder="Enter your weight" defaultValue={user.weight ?? undefined} />
+			<Textarea name="notes" placeholder="Enter your notes" />
+
+			<Button type="submit">Add workout</Button>
+		</form>
+	);
+}
