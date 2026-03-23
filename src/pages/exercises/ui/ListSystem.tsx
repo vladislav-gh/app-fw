83f42b86ebda15fa623286d7afb445b7435d3236
vladislav <vladislav.051094@yandex.ru>
2026-03-23 20:19:30 +0300

    changed: ui inside pages for exercise cards

--- src/pages/exercises/ui/ListSystem.tsx
+++ src/pages/exercises/ui/ListSystem.tsx
@@ -1,29 +1,12 @@
-import { getTranslations } from "next-intl/server";
-
 import { CardExercise, getExercisesSystem } from "@Entities/exercise";
 
 export async function ListSystem() {
-	const t = await getTranslations("exercises");
-	const tExerciseCategories = await getTranslations("exerciseCategories");
-
 	const exercisesSystem = await getExercisesSystem();
 
 	return (
 		<div className="grid gap-3 md:grid-cols-2 lg:grid-cols-3">
 			{exercisesSystem.map(exercise => (
-				<CardExercise
-					key={exercise.id}
-					exerciseId={exercise.id}
-					exerciseName={String(exercise.slug ? t(exercise.slug as Parameters<typeof t>[0]) : exercise.name)}
-					exerciseDescription={exercise.description ?? undefined}
-					exerciseCategories={exercise.categories.map(category =>
-						String(
-							category.slug
-								? tExerciseCategories(category.slug as Parameters<typeof tExerciseCategories>[0])
-								: category.name,
-						),
-					)}
-				/>
+				<CardExercise key={exercise.id} exercise={exercise} />
 			))}
 		</div>
 	);
--- src/pages/exercises/ui/ListUser.tsx
+++ src/pages/exercises/ui/ListUser.tsx
@@ -1,13 +1,11 @@
 "use client";
 
-import { useTranslations } from "next-intl";
 import { useQuery } from "@tanstack/react-query";
 
 import { CardExercise, getQueryOptionsExercisesUser } from "@Entities/exercise";
 import { useUser } from "@Entities/user";
 
 export function ListUser() {
-	const tExerciseCategories = useTranslations("exerciseCategories");
 	const user = useUser();
 
 	const { data: exercisesUser } = useQuery({
@@ -20,22 +18,8 @@ export function ListUser() {
 
 	return (
 		<div className="grid gap-3 md:grid-cols-2 lg:grid-cols-3">
-			{exercisesUser?.map(exercise => (
-				<CardExercise
-					key={exercise.id}
-					exerciseId={exercise.id}
-					exerciseName={String(exercise.name)}
-					exerciseDescription={exercise.description ?? undefined}
-					exerciseCategories={exercise.categories.map(category =>
-						String(
-							category.slug
-								? tExerciseCategories(category.slug as Parameters<typeof tExerciseCategories>[0])
-								: category.name,
-						),
-					)}
-					isEditable
-					isRemovable
-				/>
+			{exercisesUser.map(exercise => (
+				<CardExercise key={exercise.id} exercise={exercise} />
 			))}
 		</div>
 	);
