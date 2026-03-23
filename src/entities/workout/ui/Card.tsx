96b84036446d59271598662bddda1eb39ce56b20
vladislav <vladislav.051094@yandex.ru>
2026-03-23 21:56:36 +0300

    added: workout entitie card simple view for tests

--- src/entities/workout/ui/Card.tsx
+++ src/entities/workout/ui/Card.tsx
@@ -1,13 +1,39 @@
 import type { ElProps } from "@Shared/types";
+import type { Workout } from "../model";
 
+import { PAGES } from "@Shared/config";
+import { Badge, Button, Card, CardFooter, CardTitle, Link } from "@Shared/ui";
 import { cn } from "@Shared/utils";
 
-export type CardWorkoutProps = ElProps<"div">;
+export interface CardWorkoutProps extends ElProps<"div"> {
+	workout: Workout;
+}
 
-export function CardWorkout({ className, ...restProps }: CardWorkoutProps) {
+export function CardWorkout({ className, workout, ...restProps }: CardWorkoutProps) {
 	return (
-		<div className={cn("", className)} {...restProps}>
-			Card Workout
-		</div>
+		<Card className={cn("p-4 gap-2", className)} {...restProps}>
+			<CardTitle className="font-bold">{workout.id}</CardTitle>
+
+			<div>Date: {workout.date}</div>
+			<div>Duration: {workout.duration}</div>
+			<div>Notes: {workout.notes}</div>
+			<div>Weight: {workout.userWeight}</div>
+
+			{!!workout.categories.length && (
+				<div className="flex items-center flex-wrap gap-1">
+					{workout.categories.map(category => (
+						<Badge key={category} variant="secondary">
+							{category}
+						</Badge>
+					))}
+				</div>
+			)}
+
+			<CardFooter className="px-0">
+				<Button asChild>
+					<Link href={PAGES.workout(workout.id)}>View</Link>
+				</Button>
+			</CardFooter>
+		</Card>
 	);
 }
