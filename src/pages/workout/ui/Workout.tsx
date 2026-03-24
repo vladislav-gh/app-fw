f30a64d9047a5640a6a2b9548c9781ba446cbe70
vladislav <vladislav.051094@yandex.ru>
2026-03-24 20:46:29 +0300

    added: page workout show categories and exercises

--- src/pages/workout/ui/Workout.tsx
+++ src/pages/workout/ui/Workout.tsx
@@ -2,7 +2,9 @@
 
 import { notFound } from "next/navigation";
 
+import { Badge } from "@Shared/ui";
 import { useWorkout } from "@Entities/workout";
+import { FormWorkoutExerciseAdd } from "@Features/workout";
 
 export function Workout() {
 	const { workout, isLoading } = useWorkout();
@@ -17,13 +19,53 @@ export function Workout() {
 
 	return (
 		<div className="flex flex-col gap-3">
-			Workout id: {workout.id}
+			<div>Workout id: {workout.id}</div>
+
 			<div className="flex flex-col gap-2">
 				<div>Date: {workout.date}</div>
 				<div>Duration: {workout.duration}</div>
 				<div>User weight: {workout.userWeight}</div>
 				<div>Notes: {workout.notes}</div>
 			</div>
+
+			{!!workout.categories.length && (
+				<div className="flex items-center flex-wrap gap-1">
+					{workout.categories.map(category => (
+						<Badge key={category} variant="secondary">
+							{category}
+						</Badge>
+					))}
+				</div>
+			)}
+
+			{workout.exercises.map(exercise => (
+				<div key={exercise.id}>
+					{exercise.name}
+
+					{!!exercise.categories.length && (
+						<div className="flex items-center flex-wrap gap-1">
+							{exercise.categories.map(category => (
+								<Badge key={category.id} variant="secondary">
+									{category.name}
+								</Badge>
+							))}
+						</div>
+					)}
+
+					{!!exercise.sets.length && (
+						<div>
+							{exercise.sets.map(set => (
+								<div key={set.id}>
+									<div>Reps: {set.reps}</div>
+									<div>Weight: {set.weight}</div>
+								</div>
+							))}
+						</div>
+					)}
+				</div>
+			))}
+
+			<FormWorkoutExerciseAdd />
 		</div>
 	);
 }
