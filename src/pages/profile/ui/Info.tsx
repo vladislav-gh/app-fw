0caa14c65af84d2f681526959711229c0e9024ad
vladislav <vladislav.051094@yandex.ru>
2026-03-24 21:50:31 +0300

    added: page profile info edit

--- src/pages/profile/ui/Info.tsx
+++ src/pages/profile/ui/Info.tsx
@@ -1,8 +1,14 @@
 "use client";
 
+import { useState } from "react";
+
+import { Button } from "@Shared/ui";
 import { useUser } from "@Entities/user";
+import { FormUserEdit } from "@Features/user";
 
 export function Info() {
+	const [isEditing, setIsEditing] = useState(false);
+
 	const user = useUser();
 
 	if (!user) {
@@ -12,10 +18,23 @@ export function Info() {
 	return (
 		<div className="flex flex-col gap-3">
 			<div>id: {user.id}</div>
-			{user.name && <div>Name: {user.name}</div>}
-			{user.nickname && <div>Nickname: {user.nickname}</div>}
-			{user.birthDate && <div>Birthday: {user.birthDate}</div>}
-			{user.weight && <div>Weight: {user.weight}</div>}
+
+			{isEditing ? (
+				<>
+					<FormUserEdit onSuccess={() => setIsEditing(false)} />
+
+					<Button onClick={() => setIsEditing(false)}>Cancel</Button>
+				</>
+			) : (
+				<>
+					{user.name && <div>Name: {user.name}</div>}
+					{user.nickname && <div>Nickname: {user.nickname}</div>}
+					{user.birthDate && <div>Birthday: {user.birthDate}</div>}
+					{user.weight && <div>Weight: {user.weight}</div>}
+
+					<Button onClick={() => setIsEditing(true)}>Edit</Button>
+				</>
+			)}
 		</div>
 	);
 }
